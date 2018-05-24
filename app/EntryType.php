29fb159b4e3b3016aeab4c76493cb8bd5f8a7b61
Bear Hagen <b@datahjelpen.no>
2018-05-25 01:40:11 +0200

    Find EntryType by slug

--- app/EntryType.php
+++ app/EntryType.php
@@ -9,8 +9,14 @@ class EntryType extends \Illuminate\Database\Eloquent\Model
         'slug',
     ];
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function entries()
     {
         return $this->hasMany('App\Entry', 'id', 'entry_id');
     }
 }
+
