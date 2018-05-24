4ed3d99001195e5698097a8c0aac374dc7c4c1b6
Bear Hagen <b@datahjelpen.no>
2018-05-24 23:37:04 +0200

    Moved dashboard view into folder

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,4 +3,10 @@
 @section('content-main')
 	<h1>Dashboard</h1>
 	<p>Hei {{ $user->name }}</p>
+
+	@role('superadmin|admin')
+		I am a superadmin!
+	@else
+		I am not a superadmin...
+	@endrole
 @endsection
