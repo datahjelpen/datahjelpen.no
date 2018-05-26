4005ed9fffd689255209182a8b8e705bc11126dc
Bear Hagen <b@datahjelpen.no>
2018-05-26 02:16:24 +0200

    Language files for forms stuff

---
+++ resources/lang/no/forms.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'confirm' => [
+        'delete' => [
+            'ask' => 'Er du sikker på at du vil slette ":name"?',
+            'yes' => 'Ja, slett',
+            'yes-icon' => 'check',
+            'no' => 'Nei, avbryt',
+            'no-icon' => 'x'
+        ],
+        'decline' => [
+            'ask' => 'Er du sikker på at du vil avslå ":name"?',
+            'yes' => 'Ja, avslå',
+            'yes-icon' => 'check',
+            'no' => 'Nei, avbryt',
+            'no-icon' => 'x'
+        ]
+    ],
+    'select' => [
+        'singular' => [
+            'normal' => 'Velg en',
+            'please' => 'Vennligst velg en'
+        ],
+        'plural' => [
+            'normal' => 'Velg',
+            'please' => 'Vennligst velg'
+        ]
+    ]
+];
