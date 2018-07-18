ac39b37d2bbf82fc7f30d8b354f785046166c378
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-07-18 14:14:32 +0200

    Removed old files

--- js/bearwork/grid.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($) {
-	$('.grid').children('.row').each(function(index, el) {
-		var elRow = $(el);
-		var elImage = elRow.find('.bg-image');
-		setTimeout(function() {
-			elImage.height(elRow.outerHeight());
-		}, 1);
-	});
-});
