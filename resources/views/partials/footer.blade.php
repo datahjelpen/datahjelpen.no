206df12f5f8194ff23a2b1bbccbad9fe96024b25
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-07-19 01:22:01 +0200

    Added footer content. Footer styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,8 +2,40 @@
 	{{-- <img src="{{ asset('images/search-by-algolia.svg') }}" alt="Search by Algolia" width="100"> --}}
 	@yield('content-footer')
 
-	<div>
-		<p>(C) Copyright {{ Date('Y') }} - {{ config('app.name') }}</p>
+	<div id="footer-content">
+		<section>
+			<div>
+				<div class="footer-links-1">
+					<a href="{{ route('front-page') }}#services">Tjenester</a>
+					<a href="{{ route('front-page') }}#projects">Prosjekter</a>
+					<a href="{{ route('contact') }}">Kontakt</a>
+					<a href="{{ route('about') }}">Om oss</a>
+				</div>
+				<div class="footer-links-2">
+					<a href="{{ route('privacy') }}">Personvern</a>
+				</div>
+				<div class="footer-extra-info">
+					<p>Org nr: 917 406 626</p>
+				</div>
+			</div>
+			<div>
+				<div class="footer-location">
+					<p><strong>Sandefjord</strong></p>
+					<p>Søndre kullerød 2,</p>
+					<p>3241 Sandefjord</p>
+				</div>
+				<div class="footer-contact">
+					<p><strong>Kontakt</strong></p>
+					<p><span>E-post: </span><a href="mailto:post@datahjelpen.no">post@datahjelpen.no</a></p>
+					<p><span>Telefon:</span><a href="tlf:46531170">465 31 170</a></p>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="footer-copyright">
+				<p>© Copyright {{ Date('Y') }} - {{ config('app.name') }}</p>
+			</div>
+		</section>
 	</div>
 
 	@yield('scripts-before')
