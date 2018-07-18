e2df20dd6f69ed20bac7c46681bac128d00773d7
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-07-18 17:02:29 +0200

    Added front-page content structure

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,4 +1,87 @@
 @extends('partials.master')
 @section('content-main')
-    <h1>Forside</h1>
+	<header>
+    <h1>Datahjelpen</h1>
+    <p>Vi er et kreativt teknologibyrå, fokusert på å hjelpe selskaper skille seg ut i den digitale verden.</p>
+	</header>
+	<section>
+		<div>
+			<h2>Våre tjenester — Digitale opplevelser som folk elsker</h2>
+			<ul>
+				<li><a href="#">Nettsider</a></li>
+				<li><a href="#">Apps</a></li>
+				<li><a href="#">Sikkerhet</a></li>
+				<li><a href="#">Analyser</a></li>
+				<li><a href="#">SEO</a></li>
+			</ul>
+			<a href="#">
+				<span>Tjenester</span>
+				<i class="icon" data-feather="arrow-right"></i>
+			</a>
+		</div>
+	</section>
+	<section>
+		<div>
+			<h2>Referanser</h2>
+			<ul>
+				<li>
+					<img src="#" alt="bilde">
+					<h3>Takshop.no</h3>
+					<ul>
+						<li>
+							<a href="#">
+								<span>Kategori 1</span>
+								<i class="icon" data-feather="brush"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>Kategori 2</span>
+								<i class="icon" data-feather="feather"></i>
+							</a>
+						</li>
+					</ul>
+					<a href="#">Link</a>
+				</li>
+				<li>
+					<img src="#" alt="bilde">
+					<h3>Bewa.no</h3>
+					<ul>
+						<li>
+							<a href="#">
+								<span>Kategori 1</span>
+								<i class="icon" data-feather="brush"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>Kategori 2</span>
+								<i class="icon" data-feather="feather"></i>
+							</a>
+						</li>
+					</ul>
+					<a href="#">Link</a>
+				</li>
+				<li>
+					<img src="#" alt="bilde">
+					<h3>Loax.no</h3>
+					<ul>
+						<li>
+							<a href="#">
+								<span>Kategori 1</span>
+								<i class="icon" data-feather="brush"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>Kategori 2</span>
+								<i class="icon" data-feather="feather"></i>
+							</a>
+						</li>
+					</ul>
+					<a href="#">Link</a>
+				</li>
+			</ul>
+		</div>
+	</section>
 @endsection
