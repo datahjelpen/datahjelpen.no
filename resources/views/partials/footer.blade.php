30eb5c7e0477572f9109fae4d7661649f004c568
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-07-18 17:02:03 +0200

    Added footer copyright notice

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,6 +2,10 @@
 	{{-- <img src="{{ asset('images/search-by-algolia.svg') }}" alt="Search by Algolia" width="100"> --}}
 	@yield('content-footer')
 
+	<div>
+		<p>(C) Copyright {{ Date('Y') }} - {{ config('app.name') }}</p>
+	</div>
+
 	@yield('scripts-before')
 	<script src="{{ asset('js/app.js') }}"></script>
 
