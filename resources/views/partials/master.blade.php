f750c1275b05ec05d25999145315453d7c1c150f
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-07-19 21:51:40 +0200

    Fix styling for front page on other links (/home and /hjem)

--- resources/views/partials/master.blade.php
+++ resources/views/partials/master.blade.php
@@ -2,7 +2,9 @@
 @php
 	$body_class = str_replace('/', '-', Request::path());
 
-	if ($body_class == '-') $body_class = 'front-page';
+	if ($body_class == '-')    $body_class = 'front-page';
+	if ($body_class == 'home') $body_class = 'front-page';
+	if ($body_class == 'hjem') $body_class = 'front-page';
 @endphp
 <body class="{{ $body_class }}">
     @include('partials.flash-messages')
