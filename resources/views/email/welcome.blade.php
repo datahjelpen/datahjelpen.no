f59daab1a125bac11e401a595f8fe465f4085eb0
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-07-20 17:13:58 +0200

    Removed an extra } sign

--- resources/views/email/welcome.blade.php
+++ resources/views/email/welcome.blade.php
@@ -1,4 +1,4 @@
-<h1>{{ $subject }}}</h1>
+<h1>{{ $subject }}</h1>
 <p>Takk for at du registrerte deg.</p>
 <p><strong>Vi trenger deg til å bekrefte din e-postadresse.</strong></p>
 <p>Klikk på linken for å bekrefte e-postadressen din {{ route('user.verify', $email_token) }}</p>
