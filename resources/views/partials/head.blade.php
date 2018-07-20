f11873e457a9c0e7f2fe9fb73947b754901fd7bb
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-07-20 03:46:36 +0200

    Added favicon and SEO things

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,16 +8,40 @@
 	<title>{{ config('app.name') }}</title>
 	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+	<meta name="author" content="Datahjelpen AS">
+	<meta name="description" content="Vi skaper de digitale opplevelsene som folk elsker">
+	<link rel="canonical" href="https://datahjelpen.no">
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Datahjelpen AS">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://datahjelpen.no">
+	<meta property="og:image" content="https://datahjelpen.no/image.jpg">
+	<meta property="og:description" content="Vi skaper de digitale opplevelsene som folk elsker">
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@datahjelpen_no">
+	<meta name="twitter:title" content="Datahjelpen AS">
+	<meta name="twitter:description" content="Vi skaper de digitale opplevelsene som folk elsker">
+	<meta name="twitter:creator" content="@datahjelpen_no">
+	<meta name="twitter:image" content="https://datahjelpen.no/image.jpg">
+	<!-- App information -->
+	<link rel="manifest" href="/site.webmanifest">
+	<meta name="application-name" content="Datahjelpen AS">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="theme-color" content="#ff2014">
+	<meta name="msapplication-TileColor" content="#ff2014">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff2014">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<!-- Icons -->
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/manifest.json">
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#202147">
-	<meta name="theme-color" content="#ffffff">
+	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Rubik:400" rel="stylesheet">
 	<link href="https://cdn.datahjelpen.no/fonts/butler/butler-700.css" rel="stylesheet">
 	{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-72567544-8"></script> --}}
-	<script>
+{{-- 	<script>
 		window.dnt = false;
 		window.cookieConsent = false;
 
