5a40b9ae486aaa24c323d0b978908e28dfdc4f7b
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-08-03 16:44:44 +0200

    Added a context menu to Pell

--- resources/assets/js/pell.js
+++ resources/assets/js/pell.js
@@ -6,6 +6,106 @@ const createElement = tag => document.createElement(tag)
 const queryCommandState = command => document.queryCommandState(command)
 const queryCommandValue = command => document.queryCommandValue(command)
 
+const ContextMenu = function() {
+  this.showing = false;
+
+  this.contextMenu = document.createElement('div');
+  this.contextMenu.classList = 'context-menu';
+  document.documentElement.appendChild(this.contextMenu);
+
+  this.generate = function(features, context) {
+    this.contentBox = document.querySelector('.pell-content');
+
+    if (features == 'defaults') {
+      let menuOptions = document.createElement('ul');
+      menuOptions.classList.add('context-menu-options');
+
+
+      let actionBar = document.querySelector('.pell-actionbar');
+      for (var i = 0; i < actionBar.childNodes.length; i++) {
+        var showInPopUp = actionBar.childNodes[i].getAttribute('data-showinpopup');
+
+        if (showInPopUp == 'true') {
+          var option = document.createElement('li');
+          option.classList.add('context-menu-option');
+          option.appendChild(document.createTextNode(actionBar.childNodes[i].getAttribute('title')));
+          option.innerHTML = actionBar.childNodes[i].innerHTML;
+
+          (function(i, contentBox) {
+            option.onclick = event => {
+              contentBox.focus();
+              // actionBar.childNodes[i].focus();
+              actionBar.childNodes[i].click();
+            }
+          })(i, this.contentBox);
+
+          menuOptions.appendChild(option);
+        }
+      }
+
+      this.contextMenu.appendChild(menuOptions);
+    }
+
+    if (features == 'widths') {
+      let menuOptions = document.createElement('ul');
+      menuOptions.classList.add('context-menu-options');
+
+      let optionWidth100 = document.createElement('li');
+      optionWidth100.classList.add('context-menu-option');
+      optionWidth100.appendChild(document.createTextNode('Width: 100%'));
+      // if (context.target.classList.contains('content-width-100')) {
+      //   optionWidth100.classList.add('cmo-active')
+      // }
+      optionWidth100.onclick = event => {
+        context.target.classList.toggle('content-width-100');
+      }
+      menuOptions.appendChild(optionWidth100);
+
+      let optionWidthMax100 = document.createElement('li');
+      optionWidthMax100.classList.add('context-menu-option');
+      optionWidthMax100.appendChild(document.createTextNode('Width: max 100%'));
+      // if (context.target.classList.contains('content-max-width-100')) {
+      //   optionWidthMax100.classList.add('cmo-active')
+      // }
+      optionWidthMax100.onclick = event => {
+        context.target.classList.toggle('content-max-width-100');
+      }
+      menuOptions.appendChild(optionWidthMax100);
+
+      this.contextMenu.appendChild(menuOptions);
+    }
+
+    window.feather.replace();
+  }
+
+  // this.optionClick = function
+
+  this.show = function(x, y) {
+    if (!this.showing) {
+
+      if (this.contextMenu.childNodes.length > 0) {
+        this.contextMenu.classList.add('cm-open');
+      }
+
+      this.showing = true;
+    }
+
+    this.contextMenu.style.left = x+'px';
+    this.contextMenu.style.top = y+'px';
+  }
+
+  this.hide = function(x, y) {
+    this.showing = false;
+    this.contextMenu.classList.remove('cm-open');
+  }
+
+  this.clean = function() {
+    while (this.contextMenu.firstChild) {
+      this.contextMenu.removeChild(this.contextMenu.firstChild);
+    }
+  }
+}
+let pellContextMenu = new ContextMenu(); // Init
 export const exec = (command, value = null) => document.execCommand(command, false, value)
 
 const defaultActions = {
@@ -13,31 +113,46 @@ const defaultActions = {
     icon: '&#8617;',
     title: 'Undo',
     result: () => exec('undo')
-  },
+    },
   redo: {
     icon: '&#8618;',
     title: 'Redo',
     result: () => exec('redo')
+    showInPopUp: true,
+    id: 'pell-button-heading2',
+    showInPopUp: true,
+    id: 'pell-button-heading3',
+    showInPopUp: true,
+    id: 'pell-button-heading4',
+  paragraph: {
+    id: 'pell-button-paragraph',
   },
   bold: {
     icon: '<strong>B</strong>',
+    showInPopUp: false,
+    id: 'pell-button-bold',
     title: 'Bold',
     state: () => queryCommandState('bold'),
     result: () => exec('bold')
   },
   italic: {
     icon: '<em>I</em>',
+    showInPopUp: false,
+    id: 'pell-button-italic',
     title: 'Italic',
     state: () => queryCommandState('italic'),
     result: () => exec('italic')
   },
   underline: {
     icon: '<u>U</u>',
+    showInPopUp: false,
+    id: 'pell-button-underline',
     title: 'Underline',
     state: () => queryCommandState('underline'),
     result: () => exec('underline')
   },
   strikethrough: {
+    showInPopUp: false,
     icon: '<strike>S</strike>',
     title: 'Strike-through',
     state: () => queryCommandState('strikeThrough'),
@@ -62,19 +177,27 @@ const defaultActions = {
     icon: '<b>H<sub>4</sub></b>',
     title: 'Heading 4',
     result: () => exec(formatBlock, '<h4>')
-  },
+    showInPopUp: true,
+    },
   paragraph: {
     icon: '&#182;',
     title: 'Paragraph',
     result: () => exec(formatBlock, '<p>')
+    id: 'pell-button-justifyLeft',
   },
   justifyCenter: {
     icon: 'C',
+    showInPopUp: true,
+    id: 'pell-button-justifyCenter',
     title: 'Justify Center',
     result: () => exec('justifyCenter')
   },
+    showInPopUp: true,
+    id: 'pell-button-justifyRight',
   justifyFull: {
     icon: 'J',
+    showInPopUp: true,
+    id: 'pell-button-justifyFull',
     title: 'Justify Full',
     result: () => exec('justifyFull')
   },
@@ -82,11 +205,23 @@ const defaultActions = {
     icon: 'L',
     title: 'Justify Left',
     result: () => exec('justifyLeft')
-  },
+    showInPopUp: true,
+    },
   justifyRight: {
     icon: 'R',
     title: 'Justify Right',
     result: () => exec('justifyRight')
+    id: 'pell-button-ulist',
+    showInPopUp: true,
+    id: 'pell-button-olist',
+    showInPopUp: false,
+    id: 'pell-button-quote',
+    showInPopUp: false,
+    id: 'pell-button-code',
+    showInPopUp: false,
+    id: 'pell-button-link',
+    showInPopUp: true,
+    id: 'pell-button-image',
   },
   // subscript: {
   //   icon: '_',
@@ -207,15 +342,31 @@ export const init = settings => {
       setTimeout(() => exec(formatBlock, `<${defaultParagraphSeparator}>`), 0)
     }
   }
+  window.onclick = event => pellContextMenu.hide()
+  window.onblur = event => pellContextMenu.hide()
+  content.oncontextmenu = event => {
+    event.preventDefault();
+    pellContextMenu.clean();
+
+    pellContextMenu.generate('defaults', event);
+
+    if (event.target.tagName == 'IMG') {
+      pellContextMenu.generate('widths', event);
+    }
+
+    pellContextMenu.show(event.clientX, event.clientY);
+  }
   appendChild(settings.element, content)
 
   actions.forEach(action => {
     const button = createElement('button')
+    button.setAttribute('id', action.id)
     button.className = classes.button
     button.innerHTML = action.icon
     button.title = action.title
     button.setAttribute('type', 'button')
     button.onclick = () => action.result() && content.focus()
+    button.setAttribute('data-showinpopup', action.showInPopUp)
 
     if (action.state) {
       const handler = () => button.classList[action.state() ? 'add' : 'remove'](classes.selected)
