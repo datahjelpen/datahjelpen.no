001db93f78e06793fe97247986f198f37f1a2a46
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-08-21 20:44:33 +0200

    Fixed typos

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,11 +4,11 @@
 		<div class="inner-wrapper">
 			<h1 class="page-title">Om oss</h1>
 			<div class="content-text">
-				<p>Firmaet Datahjelpen AS ble stiftet januar 2016 og ligger i Sandefjord.</p>
+				<p>Firmaet Datahjelpen AS ble stiftet januar 2016 og ligger i Halden.</p>
 				<p>Datahjelpen leverer tjenester til både privat- og bedriftsmarkedet. Vi tilbyr blant annet
 				oppsett av nettsider, generell datahjelp og en-til-en opplæring. <a href="{{ route('front-page') }}#services">Les mer om våre tjenester</a></p>
 				<p>Vårt mål er å gi våre kunder en enklere IT-hverdag på en effektiv måte, til en fornuftig pris.</p>
-				<p>Ved vedlikehold av din datamaskin kan du leveres den til oss, eventuelt kan vi også være
+				<p>Ved vedlikehold av din datamaskin kan du levere den til oss, eventuelt kan vi også være
 				behjelpelige med henting. For nærmere informasjon, <a href="{{ route('contact') }}">ta kontakt</a>.</p>
 			</div>
 		</div>
