0884268a983d94a6259786e3e391193b92878cfe
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-12-13 18:13:50 +0100

    Update responsive design for NavigationBar

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -6,37 +6,46 @@
     :class="$style.root"
     :aria-expanded="this.$store.state.navigation.navExtended ? 'true' : 'false'"
   >
-    <div :class="$style.logo">
-      <slot name="logo" />
-    </div>
-    <div :class="$style.links">
-      <div :class="$style.mainLinks">
-        <slot name="links" />
+    <div :class="$style.inner">
+      <div :class="$style.logo">
+        <slot name="logo" />
       </div>
-      <div :class="$style.otherLinks">
-        <div :class="$style.otherLinksGroup">
-          <h2>{{ $t('Prosjekter') }}</h2>
-          <slot name="links" />
-        </div>
-        <div :class="$style.otherLinksGroup">
-          <h2>{{ $t('Diverse') }}</h2>
-          <slot name="links" />
-        </div>
-        <div :class="$style.otherLinksGroup">
-          <h2>{{ $t('Sosiale medier') }}</h2>
-          <slot name="links" />
-        </div>
-        <div :class="$style.otherLinksGroup">
-          <h2>{{ $t('Kontakt') }}</h2>
-          <slot name="links" />
+      <div :class="$style.links">
+        <div :class="$style.linksInner">
+          <div :class="$style.topBar">
+            <div :class="$style.logo">
+              <slot name="logo" />
+            </div>
+            <div :class="$style.close" @click="toggleMenu">
+              <CloseIcon />
+            </div>
+          </div>
+          <div :class="$style.mainLinks">
+            <slot name="links" />
+          </div>
+          <div :class="$style.otherLinks">
+            <div :class="$style.otherLinksGroup">
+              <h2>{{ $t('Prosjekter') }}</h2>
+              <slot name="links" />
+            </div>
+            <div :class="$style.otherLinksGroup">
+              <h2>{{ $t('Diverse') }}</h2>
+              <slot name="links" />
+            </div>
+            <div :class="$style.otherLinksGroup">
+              <h2>{{ $t('Sosiale medier') }}</h2>
+              <slot name="links" />
+            </div>
+            <div :class="$style.otherLinksGroup">
+              <h2>{{ $t('Kontakt') }}</h2>
+              <slot name="links" />
+            </div>
+          </div>
         </div>
       </div>
-      <div :class="$style.close" @click="toggleMenu">
-        <CloseIcon />
+      <div :class="$style.toggle" @click="toggleMenu">
+        <slot name="toggle" />
       </div>
-    </div>
-    <div :class="$style.toggle" @click="toggleMenu">
-      <slot name="toggle" />
     </div>
   </nav>
 </template>
