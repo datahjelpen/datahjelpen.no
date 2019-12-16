32faa0b72404115d3265b9415db8825d1e4c5d44
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-12-16 17:53:29 +0100

    Add ButtonGroup component

---
+++ components/ButtonGroup.vue
@@ -0,0 +1,28 @@
+<style lang="scss" module>
+.root {
+  display: block;
+
+  @media ($media-min-small) {
+    margin-left: -$space-small;
+  }
+
+  > a,
+  > button {
+    margin-top: $space-base;
+    display: block;
+
+    @media ($media-min-small) {
+      display: inline-block;
+      margin: $space-base $space-small 0;
+    }
+  }
+}
+</style>
+<template>
+  <div :class="$style.root">
+    <slot />
+  </div>
+</template>
+<script>
+export default {}
+</script>
