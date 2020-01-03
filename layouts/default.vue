7fc8660a53c5e89381c475707e590b7928ec3cb7
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-01-03 16:22:25 +0100

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -116,10 +116,138 @@
     <main id="main" :class="$style.main">
       <nuxt />
     </main>
-    <footer>
-      <a>Link 1</a>
-      <a>Link 2</a>
-      <a>Link 3</a>
+    <footer :class="$style.footer">
+      <div :class="$style.footerContent">
+        <section>
+          <div>
+            <div :class="$style.footerLinks1">
+              <a href="https://datahjelpen.no#services">Tjenester</a>
+              <a href="https://datahjelpen.no#projects">Prosjekter</a>
+              <a href="https://datahjelpen.no/kontakt">Kontakt</a>
+              <a href="https://datahjelpen.no/om">Om oss</a>
+              <a href="https://datahjelpen.no/blogg">Blogg</a>
+            </div>
+            <div :class="$style.footerLinksSocial">
+              <a href="https://fb.com/datahjelpen.no"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-facebook icon"
+                >
+                  <path
+                    d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+                  ></path></svg
+              ></a>
+              <a href="https://instagr.am/datahjelpen.no"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-instagram icon"
+                >
+                  <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                  <path
+                    d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
+                  ></path>
+                  <line x1="17.5" y1="6.5" x2="17.5" y2="6.5"></line></svg
+              ></a>
+              <a href="https://twitter.com/datahjelpen_no"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-twitter icon"
+                >
+                  <path
+                    d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+                  ></path></svg
+              ></a>
+              <a href="https://www.linkedin.com/company/datahjelpen"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-linkedin icon"
+                >
+                  <path
+                    d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                  ></path>
+                  <rect x="2" y="9" width="4" height="12"></rect>
+                  <circle cx="4" cy="4" r="2"></circle></svg
+              ></a>
+              <a href="https://github.com/datahjelpen"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-github icon"
+                >
+                  <path
+                    d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                  ></path></svg
+              ></a>
+            </div>
+            <div :class="$style.footerExtraInfo">
+              <p>Org nr: 917 406 626</p>
+            </div>
+          </div>
+          <div>
+            <div :class="$style.footerContact">
+              <p><strong>Kontakt</strong></p>
+              <p>
+                <span>E-post: </span
+                ><a href="mailto:post@datahjelpen.no">post@datahjelpen.no</a>
+              </p>
+              <p><span>Telefon:</span><a href="tlf:46531170">465 31 170</a></p>
+            </div>
+          </div>
+          <div>
+            <div :class="$style.footerLocation">
+              <p><strong>Sandefjord</strong></p>
+              <p>Rosenlundveien 1,</p>
+              <p>3225 Sandefjord</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div :class="$style.footerCopyright">
+            <p>
+              © Copyright 2016-{{ new Date().getFullYear() }} - Datahjelpen AS
+            </p>
+          </div>
+        </section>
+      </div>
     </footer>
   </div>
 </template>
