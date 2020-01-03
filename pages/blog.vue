1db24c8e3f1d090f3ec3c8727763bbabdf185600
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-01-03 16:25:59 +0100

    Add temporary pages for blog and contact

---
+++ pages/blog.vue
@@ -0,0 +1,36 @@
+<style lang="scss" module>
+.root {
+  margin: $space-large auto;
+}
+
+.card {
+  margin: 0 auto;
+  width: #{$size-base * 28}em;
+}
+</style>
+<template>
+  <Layout :class="$style.root">
+    <Card :class="$style.card">
+      <div slot="title">
+        <h1>{{ $t('Kommer snart ...') }}</h1>
+      </div>
+    </Card>
+  </Layout>
+</template>
+<script>
+import Layout from '../components/Layout'
+import Card from '../components/Card'
+
+export default {
+  components: {
+    Layout,
+    Card
+  },
+  nuxtI18n: {
+    paths: {
+      en: '/blog',
+      nb: '/blogg'
+    }
+  }
+}
+</script>
