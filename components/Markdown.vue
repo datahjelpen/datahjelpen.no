15e25f6db89421cc2e26e7cc3c31cc365013e821
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-01-06 17:32:48 +0100

    Add component that renders markdown

---
+++ components/Markdown.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-html="markdown">
+    <slot />
+  </div>
+</template>
+<script>
+import MarkdownIt from 'markdown-it'
+
+const md = new MarkdownIt({
+  breaks: true
+})
+export default {
+  data() {
+    return {
+      markdown: md.render(this.$slots.default[0].text)
+    }
+  }
+}
+</script>
