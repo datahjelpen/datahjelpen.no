6706905b43d2fa0aacd4b99d8e1a64407303caf4
Bjørnar Hagen <bjornar@datahjelpen.no>
2020-01-10 15:28:44 +0100

    Fix spacing for headings inside Layout component

--- components/Layout.vue
+++ components/Layout.vue
@@ -3,6 +3,15 @@ p:first-of-type:last-of-type {
   margin-bottom: 0;
 }
 
+h1:first-child,
+h2:first-child,
+h3:first-child,
+h4:first-child,
+h5:first-child,
+h6:first-child {
+  margin-top: 0;
+}
+
 .layoutdefault,
 .layoutcols-1-3,
 .layoutcols-3-1,
