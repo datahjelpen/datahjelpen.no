34ad9805d4d376d81a581b8562dea2fd595015d3
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-03-17 23:16:10 +0100

    Add page with information about the clientweb page

---
+++ pages/clientweb.vue
@@ -0,0 +1,48 @@
+<style lang="scss" module>
+.root {
+  margin: $space-large auto;
+}
+
+.card {
+  margin: 0 auto;
+  width: #{$size-base * 28}em;
+}
+</style>
+<template>
+  <Layout :class="$style.root">
+    <Card :class="$style.card">
+      <div slot="title">
+        <h1>{{ $t('Datahjelpen kundeweb') }}</h1>
+      </div>
+      <div slot="content">
+        <p>Denne siden er midlertidig nede grunnet oppgradering av systemet.</p>
+        <p>
+          Trenger du hjelp med noe?
+          <nuxt-link slot="links" :to="localePath('contact')"
+            >Kontakt oss her
+          </nuxt-link>
+        </p>
+      </div>
+    </Card>
+  </Layout>
+</template>
+<script>
+import Layout from '../components/Layout'
+import Card from '../components/Card'
+
+export default {
+  head: {
+    title: 'Clientweb - Datahjelpen AS'
+  },
+  components: {
+    Layout,
+    Card
+  },
+  nuxtI18n: {
+    paths: {
+      en: '/clientweb',
+      nb: '/clientweb'
+    }
+  }
+}
+</script>
