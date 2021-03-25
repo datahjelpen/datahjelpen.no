80c9f164c965234c20eb6404805e0c617d597381
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-03-25 12:10:51 +0100

    Update privacy policy.

---
+++ components/PrivacyText.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <h1>Personvernerklæring</h1>
+    <p>
+      Denne personvernerklæringen gjelder for nettsiden til Datahjelpen AS, org.nr.: 917 406 626
+    </p>
+    <p>
+      <strong>Sist oppdatert:</strong>
+      <time datetime="2021-03-25">25. {{ $t('mars') }} 2021</time>
+    </p>
+    <p><strong>Siste endringer:</strong></p>
+    <ul>
+      <li>Oppdaterte informasjon angående hvordan man kan slette sine data.</li>
+      <li>Oppdaterte informasjon angående personopplysninger som behandles.</li>
+      <li>Oppdaterte bedriften sin addresse.</li>
+      <li>La til seksjon for siste endringer.</li>
+    </ul>
+
+    <h2>Innledning</h2>
+    <p>
+      Personvernerklæringen inneholder informasjon om hvilke opplysninger som innhentes, hvorfor de innhentes, hvordan
+      innhentede opplysninger blir brukt og hvilke alternativer og rettigheter du har vedrørende innhenting av disse
+      personopplysningene. Dokumentet er bevisst forsøkt å holde et enkelt og forståelig språk. Dersom noe er uklart
+      hører vi gjerne fra deg på
+      <a href="mailto:personvern@datahjelpen.no">personvern@datahjelpen.no</a>.
+    </p>
+
+    <h2>1. Personopplysninger som behandles</h2>
+    <p>
+      Datahjelpen AS samler ikke inn personopplysninger som fødselsdato- og personnummer, eller opplysninger om
+      personlige betalingskort.
+    </p>
+    <p>Følgende informasjon blir innhentet og/eller lagret:</p>
+    <ul>
+      <li>Firmanavn, fullt navn</li>
+      <li>Firmaadresse, fakturaadresse</li>
+      <li>Telefonnummer, e-postadresse</li>
+      <li>Historikk over dine tidligere kjøp hos oss</li>
+      <li>
+        Kommunikasjonshistorikk. Mellom deg og oss på e-post, chat, eller via sosiale medier.
+      </li>
+    </ul>
+
+    <h2>2. Hvor henter vi personopplysninger fra?</h2>
+    <p>
+      Datahjelpen AS samler inn opplysninger gjennom skjemaer på vår nettside, samt. forespørsler gjennom telefon,
+      e-post og chat via våre sosiale medier. Det er frivillig å oppgi denne informasjonen. Hvis du velger å ikke oppgi
+      personopplysningene, kan det hindre oss i å gi deg tilgang til etterspurt service, produkter eller tjenester.
+    </p>
+
+    <h2>3. Formålet med behandlingen</h2>
+    <p>
+      Datahjelpen AS lagrer kun informasjon som er nødvendig for å kunne innfri våre forpliktelser i henhold til
+      inngåtte avtaler og for å kunne administrere ditt kundeforhold med oss. Bruken er begrenset til det som er strengt
+      nødvendig ut fra hvilken tjeneste vi skal yte eller har behov for. Personopplysningene benytter vi til disse
+      formålene:
+    </p>
+    <ul>
+      <li>Kundeadministrasjon, fakturering og levering av tjenester</li>
+      <li>Kundeoppfølging</li>
+    </ul>
+
+    <h2>4. Hvordan lagrer vi informasjonen</h2>
+    <p>
+      Informasjon sendt gjennom skjema på datahjelpen.no lagres i vårt e-post system (Microsoft 365).
+    </p>
+
+    <h2>5. Hvor lenge lagres informasjonen?</h2>
+    <p>
+      Opplysningene lagres hos oss eller hos tredjeparter vi har avtale med så lenge du har aktivt kundeforhold eller så
+      lenge opplysningen er relevante og vi har behov for opplysningene. F.eks. vil personopplysninger knyttet til
+      fakturering bli lagret i henhold til bokføringsloven.
+    </p>
+
+    <h2>6. Hvordan slette opplysningene?</h2>
+    <p>
+      Informasjon slettes på oppfordring ved å kontakte vårt kundesenter. Kunder med brukerkonto hos oss kan selv slette
+      sine opplysninger via vår
+      <a href="https://clientweb.datahjelpen.no/" target="_blank">kundeweb (https://clientweb.datahjelpen.no/)</a>
+    </p>
+
+    <h2>7. Utlevering av opplysninger til tredjeparter</h2>
+    <p>Personopplysningene deles ikke med utenforstående tredjeparter.</p>
+
+    <h2>8. Dine rettigheter</h2>
+    <p>
+      Dersom du ønsker å vite hvilken informasjon vi har registrert på deg kan du ta kontakt med oss på
+      <a href="mailto:personvern@datahjelpen.no">personvern@datahjelpen.no</a>
+    </p>
+    <ul>
+      <li>
+        <strong>Innsyn</strong>
+        <p>
+          Vi plikter til å kostnadsfritt å gi deg innsyn og informasjon om registrerte opplysninger vi har om deg innen
+          30 dager.
+        </p>
+      </li>
+      <li>
+        <strong>Rettelser</strong>
+        <p>
+          Oppdager du feil eller det er endringer i opplysningene (f.eks. ny adresse, telefonnummer, e-post) ber vi deg
+          vennligst ta kontakt med oss snarest mulig, slik at vi kan oppdatere opplysningene.
+        </p>
+      </li>
+      <li>
+        <strong>Dataportabilitet</strong>
+        <p>
+          Du har muligheten til å kreve personopplysningene dine utlevert i et strukturert, alminnelig anvendt og
+          maskinlesbart format, og til å ta opplysningene med deg til en annen virksomhet. Dette gjelder kun
+          opplysninger du aktivt har gitt fra deg.
+        </p>
+      </li>
+      <li>
+        <strong>Begrenset behandling</strong>
+        <p>
+          Ønsker du å begrense vår behandlingen av dine persondata, har du rett til å be om dette.
+        </p>
+      </li>
+      <li>
+        <strong>Sletting</strong>
+        <p>
+          Ønsker du å slette dine personopplysninger, har du rett til å gjøre dette så lenge vi ikke er lovpålagt å
+          lagre de.
+        </p>
+      </li>
+    </ul>
+    <p></p>
+    <p>
+      Dersom du mener at Datahjelpen AS ikke har overholdt dine rettigheter i henhold til personopplysningsloven, har du
+      rett til å klage til den aktuelle tilsynsmyndigheten. Dette gjøres ved å sende klage til Datatilsynet,
+      kontaktinformasjon er tilgjengelig på
+      <a href="https://datatilsynet.no" target="_blank">www.datatilsynet.no</a>
+    </p>
+
+    <h2>9. Slik kontakter du oss</h2>
+    <p>Telefon: <a href="tel:+47 465 31 170">+47 465 31 170</a></p>
+    <p>
+      E-post:
+      <a href="mailto:personvern@datahjelpen.no">personvern@datahjelpen.no</a>
+    </p>
+    <p>Datahjelpen AS, Hunsrødkroken 7 3231 Sandefjord.</p>
+  </div>
+</template>
